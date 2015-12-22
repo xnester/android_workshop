493ea9ebcddb7dcd107e95b7bf88a91eac4555f7
BBB-spornchai <si.pornchai@gmail.com>
2015-12-22 10:31:45 +0700

    new my list view

--- app/src/main/java/vm/demo/spornchai/traffic/MainActivity.java
+++ app/src/main/java/vm/demo/spornchai/traffic/MainActivity.java
@@ -1,13 +1,54 @@
 package vm.demo.spornchai.traffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    } //Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Play media
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.bird_maxican);
+                buttonMediaPlayer.start();
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://www.beenets.com"));
+                startActivity(objIntent);
+
+            } //event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+
+    }
+} //Main Class
